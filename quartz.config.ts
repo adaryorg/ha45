0959e21bba96becc9a9c271685cb477d6ee73a8c
Yuval Adar <adar.yuval@gmail.com>
2025-03-27 05:36:11 +0200

    Quartz sync: Mar 27, 2025, 5:36 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: 'G-C02CSD67V8',
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
