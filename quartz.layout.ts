a0b0d888ac673b4a599a7b2d20e91a132b1dbdea
Yuval Adar <yadar@zscaler.com>
2025-03-28 13:34:00 +0300

    Quartz sync: Mar 28, 2025, 1:34 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Email: "mailto:ha45@ha45.org",
+      "Discord Community": "https://discord.gg/fPEUghFU",
     },
   }),
 }
