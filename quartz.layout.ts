58696159410c46b7d8aad0adf2ef5cbc398c6a0e
Yuval Adar <yadar@zscaler.com>
2025-10-12 11:05:22 +0300

    Quartz sync: Oct 12, 2025, 11:05 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,7 @@ const recentNotes = [
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
+  afterBody: [...recentNotes.map((c) => Component.MobileOnly(c))],
   Component.Comments({
     provider: 'giscus',
     options: {
