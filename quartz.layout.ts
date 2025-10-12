f531bc2692cc17cb20ac372f6e6e0623c30cca1e
Yuval Adar <yadar@zscaler.com>
2025-10-12 10:32:35 +0300

    Quartz sync: Oct 12, 2025, 10:32 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,7 +1,32 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
-// components shared across all pages
+const recentNotes = [
+  Component.RecentNotes({
+    title: "Recent Articles",
+    limit: 3,
+    filter: (f) =>
+      f.slug!.startsWith("Articles/") && f.slug! !== "Articles/index" && !f.frontmatter?.noindex,
+    linkToMore: "Articles/" as SimpleSlug,
+    showTags: false,
+  }),
+  Component.RecentNotes({
+    title: "Recent nutrition data",
+    limit: 2,
+    filter: (f) => f.slug!.startsWith("nutrition_log"),
+    linkToMore: "nutrition_logi/" as SimpleSlug,
+    showTags: false,
+  }),
+  Component.RecentNotes({
+    title: "Recent execrise data",
+    limit: 2,
+    filter: (f) => f.slug!.startsWith("daily_logs/"),
+    linkToMore: "daily_logs/" as SimpleSlug,
+    showTags: false,
+  }),
+]
+
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
@@ -28,56 +53,43 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+const left = [
+  Component.PageTitle(),
+  Component.MobileOnly(Component.Spacer()),
+  Component.Flex({
+    components: [
+      {
+        Component: Component.Search(),
+        grow: true,
+      },
+      { Component: Component.Darkmode() },
+    ],
+  }),
+  ...recentNotes.map((c) => Component.DesktopOnly(c)),
+]
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+  left,
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        showTags: false,
+      },
+      globalGraph: {
+        showTags: false,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.RecentNotes({ title: "Recent Writings", limit: 5 }),
   ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
+  left,
   right: [],
 }
+
